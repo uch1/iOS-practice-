2bb2f579d1bdad6dc4e5e16706bdd1140a4ce018
Uchenna  Aguocha <uk.aguocha@gmail.com>
2017-12-19 00:59:47 -0600

    Added autolayout for prev button

--- AutolayoutPractice/AutolayoutPractice/ViewController.swift
+++ AutolayoutPractice/AutolayoutPractice/ViewController.swift
@@ -22,10 +22,8 @@ class ViewController: UIViewController {
     
     let descriptionTextView: UITextView = {
         let textView = UITextView()
-        
         let attributeText = NSMutableAttributedString(string: "Join us today in our fun and games!", attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 18)])
         attributeText.append(NSAttributedString(string: "\n\n\nAre you ready for loads and loads of fun? Don't wait any longer! We hope to see you in our stores soon.", attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 13), NSAttributedStringKey.foregroundColor: UIColor.gray]))
-        
         textView.attributedText = attributeText
 //        textView.text = "Join us today in our fun and games!"
 //        textView.font = UIFont.boldSystemFont(ofSize: 18)
@@ -36,15 +34,37 @@ class ViewController: UIViewController {
         return textView
     }()
     
+    // make sure to apple the correct encapsulation principles in your classes
+    private let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Prev", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //view.addSubview(thumbImageView)
         view.addSubview(descriptionTextView)
         
+        setupBottomControls()
         setupAutoLayout()
     }
 
+    fileprivate func setupBottomControls() {
+        view.addSubview(previousButton)
+        previousButton.backgroundColor = .red
+     //   previousButton.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        NSLayoutConstraint.activate([
+//                previousButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                previousButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+                previousButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                previousButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                previousButton.heightAnchor.constraint(equalToConstant: 50)
+            ])
+    }
+    
     private func setupAutoLayout() {
         // positioning an ImageView based on the
         // dimensions of the view controller
@@ -64,14 +84,7 @@ class ViewController: UIViewController {
         thumbImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
         thumbImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
         topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
-        
-        
 
-        
-        
-        
-        
-        
 //        thumbImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
 //        thumbImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
 //        thumbImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
