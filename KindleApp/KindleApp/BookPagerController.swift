d8d34b920dfcb0b75cff5fa8d268f1a6feb24b6b
Uchenna  Aguocha <uk.aguocha@gmail.com>
2017-12-26 14:42:20 -0600

    Transform cells to pages

--- KindleApp/KindleApp/BookPagerController.swift
+++ KindleApp/KindleApp/BookPagerController.swift
@@ -10,17 +10,31 @@ import UIKit
 
 class BookPagerController: UICollectionViewController {
     let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.title = "Book"
+        setupCollectionViewLayout()
+    }
+    
+    func setupCollectionViewLayout() {
         collectionView?.backgroundColor = .white
         collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
-        navigationItem.title = "Book"
+        collectionView?.isPagingEnabled = true
+        
+        let layout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout
+        layout?.scrollDirection = .horizontal
+        layout?.minimumLineSpacing = 0
     }
     
 }
 
-extension BookPagerController {
+extension BookPagerController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
@@ -28,7 +42,12 @@ extension BookPagerController {
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = .red
+        
+        if indexPath.item % 2 == 0 {
+            cell.backgroundColor = .red
+        } else {
+            cell.backgroundColor = .blue
+        }
         
         return cell
     }
