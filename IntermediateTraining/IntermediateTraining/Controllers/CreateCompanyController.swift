b9bf0d4d903807085db943ed122b9a4f1eb07acf
Uchenna  Aguocha <uk.aguocha@gmail.com>
2018-01-13 21:45:47 -0800

    Created an Employee Entity in Core Data

--- IntermediateTraining/IntermediateTraining/Controllers/CreateCompanyController.swift
+++ IntermediateTraining/IntermediateTraining/Controllers/CreateCompanyController.swift
@@ -132,11 +132,13 @@ class CreateCompanyController: UIViewController, UIImagePickerControllerDelegate
     }
     
     func setupUI() {
-        view.addSubview(lightBlueBackgroundView)
-        lightBlueBackgroundView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        lightBlueBackgroundView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        lightBlueBackgroundView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        lightBlueBackgroundView.heightAnchor.constraint(equalToConstant: 350).isActive = true
+        let lightBlueBackgroundView = setupLightBlueBackgroundView(height: 350)
+        
+//        view.addSubview(lightBlueBackgroundView)
+//        lightBlueBackgroundView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+//        lightBlueBackgroundView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+//        lightBlueBackgroundView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+//        lightBlueBackgroundView.heightAnchor.constraint(equalToConstant: 350).isActive = true
         
         view.addSubview(companyImageView)
         companyImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 8).isActive = true
--- IntermediateTraining/IntermediateTraining/CoreDataManager.swift
+++ IntermediateTraining/IntermediateTraining/CoreDataManager.swift
@@ -12,7 +12,7 @@ struct CoreDataManager {
     
     static let shared = CoreDataManager() // will live forever as long as your application is stil alive, and it's properties will too
     
-    let presistentContainer: NSPersistentContainer = {
+    let persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "IntermediateTrainingModels")
         container.loadPersistentStores { (storeDescription, error) in
             if let error = error {
@@ -24,7 +24,7 @@ struct CoreDataManager {
     
     func fetchCompanies() -> [Company] {
         
-        let context = presistentContainer.viewContext
+        let context = persistentContainer.viewContext
         
         let fetchRequest = NSFetchRequest<Company>(entityName: "Company")
         
@@ -38,5 +38,25 @@ struct CoreDataManager {
         
     }
     
+    // LOOK HERE 
+    func createEmployee(employeeName: String) -> Error? {
+        let context = persistentContainer.viewContext
+        
+        // create employee
+        let employee = NSEntityDescription.insertNewObject(forEntityName: "Employee", into: context)
+        
+        employee.setValue(employeeName, forKey: "name")
+        
+        do {
+            try context.save()
+            // save succeeds
+            return nil
+            
+        } catch let error {
+            print("Failed to create employee:", error)
+            return error
+        }
+        
+    }
     
 }
