e83fa95f72754446c177f9d28ffddbe002d8f2b9
Uchenna  Aguocha <uk.aguocha@gmail.com>
2018-01-26 01:16:19 -0800

    Added CAAnimation

--- CircularProgressBar/CircularProgressBar/ViewController.swift
+++ CircularProgressBar/CircularProgressBar/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let shapeLayer = CAShapeLayer()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // let's start by drawing a circle somehow
-        let shapeLayer = CAShapeLayer()
         
         let center = view.center
         let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
@@ -31,6 +33,14 @@ class ViewController: UIViewController {
 
     @objc private func handleTap() {
         print("Attempting to animate stroke")
+        
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 2
+        basicAnimation.fillMode = kCAFillModeForwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        shapeLayer.add(basicAnimation, forKey: "basic")
     }
 
 }
