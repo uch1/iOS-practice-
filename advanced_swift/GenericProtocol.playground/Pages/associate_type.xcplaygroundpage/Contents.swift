ab3bc832fe2ae2216c126148dcee897e777e5cdf
Uchenna Aguocha <uk.aguocha@gmail.com>
2018-04-16 22:33:39 -0700

    Associative Types

---
+++ advanced_swift/GenericProtocol.playground/Pages/associate_type.xcplaygroundpage/Contents.swift
@@ -0,0 +1,47 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+    Generic Protocols
+    Intro to Associate Type
+ 
+ Problem: How to create a generic protocol
+*/
+
+
+// NOTE: Type must be defined before compiled 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//: [Next](@next)
